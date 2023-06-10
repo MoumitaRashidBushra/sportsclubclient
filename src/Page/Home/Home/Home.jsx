eb72eda5cc9ecef88391e8c393cb87e5ee35d1db
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2023-06-11 02:49:22 +0600

    Home instructor  add

--- src/Page/Home/Home/Home.jsx
+++ src/Page/Home/Home/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Slider from '../Slider/Slider';
 import Banner from '../Banner/Banner';
 
+import InstructorHome from './InstructorHome';
+
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <InstructorHome></InstructorHome>
         </div>
     );
 };
---
+++ src/Page/Home/Home/InstructorHome.jsx
@@ -0,0 +1,46 @@
+import React, { useContext } from 'react';
+import ShowIntructor from '../../InstructorsPage/ShowIntructor';
+import { AuthContext } from '../../../Provider/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
+import InShowHome from './InShowHome';
+
+const InstructorHome = () => {
+    const { user, updateUser, loading } = useContext(AuthContext);
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
+        const res = await fetch('http://localhost:5000/home')
+        return res.json();
+
+    })
+    console.log(users)
+    return (
+        <div>
+            <section>
+                <div className='lg:container lg:mx-auto px-5 lg:px-20 lg:pt-10 pt-10 '>
+                    <h2 className='text-center text-4xl font-bold pt-8 pb-16'> All Instructor!!</h2>
+                    <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-10  mb-16 lg:px-20 px-5'>
+
+
+
+                        {
+                            users.map(instructors => <ShowIntructor
+                                key={instructors._id}
+                                instructors={instructors}>
+
+                            </ShowIntructor>)
+                        }
+
+
+
+
+
+
+
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    );
+};
+
+export default InstructorHome;
